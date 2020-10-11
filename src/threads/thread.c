75d1c333c5aa1a83bcbf3063ec8ef35d5ad0102a
hajae <rntlqvnf@naver.com>
2020-10-11 16:59:19 +0900

    <Chore>: Implement priority scheduling
    Not priority donation part.
    Simply implemented priority scheduling only.
    Feature are..
    - When change into ready, insert by priority
    - When new thread created, thread_yield() to change to highest priority
    - When priority changed, if lower than previous, thread_yield()

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -200,6 +200,7 @@ thread_create (const char *name, int priority,
 
   /* Add to run queue. */
   thread_unblock (t);
+  thread_yield();
 
   return tid;
 }
@@ -237,7 +238,7 @@ thread_unblock (struct thread *t)
 
   old_level = intr_disable ();
   ASSERT (t->status == THREAD_BLOCKED);
-  list_push_back (&ready_list, &t->elem);
+  list_insert_ordered(&ready_list,  &t->elem, priority_compare, NULL);
   t->status = THREAD_READY;
   intr_set_level (old_level);
 }
@@ -308,7 +309,7 @@ thread_yield (void)
 
   old_level = intr_disable ();
   if (cur != idle_thread) 
-    list_push_back (&ready_list, &cur->elem);
+    list_insert_ordered(&ready_list, &cur->elem, priority_compare, NULL);
   cur->status = THREAD_READY;
   schedule ();
   intr_set_level (old_level);
@@ -335,7 +336,10 @@ thread_foreach (thread_action_func *func, void *aux)
 void
 thread_set_priority (int new_priority) 
 {
+  int prev_priority = thread_current ()->priority;
   thread_current ()->priority = new_priority;
+  if(prev_priority > new_priority)
+    thread_yield();
 }
 
 /* Returns the current thread's priority. */
@@ -578,6 +582,15 @@ allocate_tid (void)
 
   return tid;
 }
+
+bool 
+priority_compare(const struct list_elem* a, const struct list_elem* b, void* aux)
+{
+  struct thread *thread_a = list_entry(a, struct thread, elem);
+  struct thread *thread_b = list_entry(b, struct thread, elem);
+  return thread_a->priority < thread_b->priority;
+}
+
 
 /* Offset of `stack' member within `struct thread'.
    Used by switch.S, which can't figure it out on its own. */
