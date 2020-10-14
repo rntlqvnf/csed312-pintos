e95ec5bad25226dd455624dbabf1615068d0547f
hajae <rntlqvnf@naver.com>
2020-10-14 10:34:54 +0900

    <Refactor>: Remove redundant insert_ordered

--- src/threads/thread.h
+++ src/threads/thread.h
@@ -143,8 +143,6 @@ void thread_set_nice (int);
 int thread_get_recent_cpu (void);
 int thread_get_load_avg (void);
 
-void ready_list_sort(void);
-
 bool priority_compare(const struct list_elem* a, const struct list_elem* b, void* aux);
 
 #endif /* threads/thread.h */
