2960f47be337bb262e4ba53428196131dfe9f323
hajae <rntlqvnf@naver.com>
2020-11-09 23:35:30 +0900

    Feat: Implement sys write
    Implemented sys wrtie.
    This is because we have to print user process's result.
    I didn't implemented it, and test failed.
    After I implement, test passes!
    But I dont' know why I should esp+5 6 7..

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -1,10 +1,27 @@
 #include "userprog/syscall.h"
 #include <stdio.h>
 #include <syscall-nr.h>
+#include <user/syscall.h>
 #include "threads/interrupt.h"
 #include "threads/thread.h"
+#include "threads/vaddr.h"
+#include "process.h"
 
 static void syscall_handler (struct intr_frame *);
+void validate_addr(const void* vaddr);
+
+void halt();
+void exit(int status);
+pid_t exec(const char* cmd_line);
+int wait(pid_t pid);
+bool create(const char* file, unsigned initial_size);
+bool remove(const char* file);
+int open(const char* file);
+int filesize(int fd);
+int write(int fd, const void* buffer, unsigned size);
+void seek(int fd, unsigned position);
+unsigned tell(int fd);
+void close(int fd);
 
 void
 syscall_init (void) 
@@ -21,12 +38,20 @@ syscall_handler (struct intr_frame *f UNUSED)
   switch (syscall_num)
   {
     case SYS_HALT:
+      halt();
       break;
     case SYS_EXIT:
+      validate_addr(esp+1);
+      exit(*(esp+1));
       break;
     case SYS_EXEC:
+      validate_addr(esp+1);
+      validate_addr(*(esp+1));
+      f->eax = exec(*(esp+1));
       break;
     case SYS_WAIT:
+      validate_addr(esp+1);
+      f->eax = wait(*(esp+1));
       break;
     case SYS_CREATE:
       break;
@@ -39,6 +64,10 @@ syscall_handler (struct intr_frame *f UNUSED)
     case SYS_READ:
       break;
     case SYS_WRITE:
+      validate_addr(esp+5);
+      validate_addr(esp+6);
+      validate_addr(esp+7);
+      f->eax = write(*(esp+5), *(esp+6), *(esp+7));
       break;
     case SYS_SEEK:
       break;
@@ -47,7 +76,63 @@ syscall_handler (struct intr_frame *f UNUSED)
     case SYS_CLOSE:
       break;
     default:
-      print("Not implemented \n");
+      printf("Not implemented \n");
       break;
+  }
+}
+
+void
+validate_addr(const void* vaddr)
+{
+  if(is_kernel_vaddr(vaddr))
+  {
+    exit(-1);
+    return;
+  }
+
+	if(!pagedir_get_page(thread_current()->pagedir, vaddr)) //page fault
+  {
+    exit(-1);
+    return;
+  }
+}
+
+void
+halt()
+{
+  shutdown_power_off();
+}
+
+void
+exit(int status)
+{
+  printf("%s: exit(%d)\n", thread_name(), status);
+  thread_exit();
+}
+
+pid_t 
+exec(const char* cmd_line)
+{
+  return process_execute(cmd_line);
+}
+
+int 
+wait(pid_t pid)
+{
+  return process_wait(pid);
+}
+
+int 
+write(int fd, const void* buffer, unsigned size)
+{
+  if(fd == 1)
+  {
+    putbuf(buffer, size);
+    return size;
+  }
+  else
+  {
+    // TODO: File write
+    return -1;
   }
 }
